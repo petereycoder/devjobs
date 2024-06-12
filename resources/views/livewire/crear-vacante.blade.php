a957b02b4dbb993afa98bb8a2c03f1f1566737b6
petereycoder <fp.pipe.reyes@gmail.com>
2024-06-12 17:52:41 -0600

    Se muestra una imagen de preview con livewire

--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -92,6 +92,12 @@
             wire:model="imagen" 
             accept="image/*"
         />
+        <div class="my-5 w-80">
+            @if ($imagen)
+                Imagen:
+                <img src="{{ $imagen->temporaryUrl() }}" >
+            @endif
+        </div>
         <x-input-error :messages="$errors->get('imagen')" class="mt-2" />
     </div>
 
