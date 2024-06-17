86c21a7d6c6ac75050cce9a2492634d14126eace
petereycoder <fp.pipe.reyes@gmail.com>
2024-06-17 11:35:46 -0600

    Se implementó todo lo necesario para guardar editar la información de la vacante

--- app/Livewire/EditarVacante.php
+++ app/Livewire/EditarVacante.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Carbon;
 
 class EditarVacante extends Component
 {
+    public $vacante_id;
     public $titulo;
     public $salario;
     public $categoria;
@@ -28,6 +29,7 @@ class EditarVacante extends Component
     ];
 
     public function mount(Vacante $vacante){
+        $this->vacante_id = $vacante->id;
         $this->titulo = $vacante->titulo;
         $this->salario = $vacante->salario_id;
         $this->categoria = $vacante->categoria_id;
@@ -39,6 +41,27 @@ class EditarVacante extends Component
 
     public function editarVacante(){
         $datos = $this->validate();
+
+        //Si hay una nueva imagen
+
+        //Encontrar la vacante a editar
+        $vacante = Vacante::find($this->vacante_id);
+
+        //Asignar los valores
+        $vacante->titulo = $datos['titulo'];
+        $vacante->salario_id = $datos['salario'];
+        $vacante->categoria_id = $datos['categoria'];
+        $vacante->empresa = $datos['empresa'];
+        $vacante->ultimo_dia = $datos['ultimo_dia'];
+        $vacante->descripcion = $datos['descripcion'];
+
+        //Guardar la vacante
+        $vacante->save();
+
+        //Redireccionar
+        session()->flash('mensaje', 'La vacante se actualizó correctamente');
+
+        return redirect()->route('vacantes.index');
     }
 
     public function render()
