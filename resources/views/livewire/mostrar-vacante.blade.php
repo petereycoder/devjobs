1ba1ef3b264c930c79eadef091b2c6dd981a2846
petereycoder <fp.pipe.reyes@gmail.com>
2024-06-18 13:22:41 -0600

    Se muestra mensaje al usuario de crear cuenta si desea aplicar a la vacante

--- resources/views/livewire/mostrar-vacante.blade.php
+++ resources/views/livewire/mostrar-vacante.blade.php
@@ -31,4 +31,13 @@
             <p>{{$vacante->descripcion}}</p>
         </div>
     </div>
+    @guest
+        <div class="mt-5 bg-gray-50 border border-dashed p-5 text-center">
+            <p>
+                ¿Deseas aplicar a esta vacante? <a class="font-bold text-indigo-600" href="{{ route('register') }}">
+                    Obten una cuenta, aplica a esta y otras vacantes
+                </a>
+            </p>
+        </div>
+    @endguest
 </div>
