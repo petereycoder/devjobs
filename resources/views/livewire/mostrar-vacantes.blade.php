a15791afe3dfce924478bf8513c646446aab9387
petereycoder <fp.pipe.reyes@gmail.com>
2024-06-18 09:32:50 -0600

    Se añadió sweet Alert2 para eliminar vacante

--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -39,5 +39,29 @@
     </div>
 </div>
 
+@push('scripts')
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        Swal.fire({
+            title: "¿Eliminar Vacante?",
+            text: "Una vacante eliminada no se puede recuperar",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Si, ¡Eliminar!",
+            cancelButtonText: "Cancelar"
+        }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: "¡Eliminado!",
+                text: "La vacante ha sido eliminado",
+                icon: "success"
+            });
+        }
+        });
+    </script>
+@endpush
+
 
 
