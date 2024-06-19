d1ff9825f165fb3c4d7a71fff61aab9c73e1e5ff
petereycoder <fp.pipe.reyes@gmail.com>
2024-06-19 10:15:13 -0600

    Se validó el formulario

--- app/Livewire/PostularVacante.php
+++ app/Livewire/PostularVacante.php
@@ -6,6 +6,24 @@ use Livewire\Component;
 
 class PostularVacante extends Component
 {
+    public $cv;
+
+    protected $rules = [
+        'cv' => 'required|mimes:pdf'
+    ];
+
+    public function postularme(){
+        $this->validate();
+
+        //Almacenar CV en el disco duro
+
+        //Crear la vacante
+
+        //Crear notificación y enviar al email
+
+        //Mostrar al usuario un mensaje de ok
+    }
+
     public function render()
     {
         return view('livewire.postular-vacante');
--- resources/views/livewire/postular-vacante.blade.php
+++ resources/views/livewire/postular-vacante.blade.php
@@ -1,10 +1,10 @@
 <div class="bg-gray-100 p-5 mt-10 flex flex-col justify-center items-center">
     <h3 class="text-center text-2xl font-bold my-4">Postularme a esta vacante</h3>
 
-    <form class="w-96 mt-5">
+    <form wire:submit.prevent='postularme' class="w-96 mt-5">
         <div class="mb-4">
             <x-input-label for="cv" :value="__('Curriculum o Hoja de Vida (PDF)')" />
-            <x-text-input id="cv" type="file" accept=".pdf" class="block mt-1 w-full" />
+            <x-text-input id="cv" type="file" wire:model="cv" accept=".pdf" class="block mt-1 w-full" />
             <x-input-error :messages="$errors->get('cv')" class="mt-2" />
         </div>
 
