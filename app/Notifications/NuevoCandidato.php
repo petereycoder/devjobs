12312d0cf4f96873105a387321fd43e1dfbdd94f
petereycoder <fp.pipe.reyes@gmail.com>
2024-06-20 11:03:33 -0600

    Se implementó la notificación al reclutador

--- app/Notifications/NuevoCandidato.php
+++ app/Notifications/NuevoCandidato.php
@@ -11,12 +11,19 @@ class NuevoCandidato extends Notification
 {
     use Queueable;
 
+    private $id_vacante;
+    private $nombre_vacante;
+    private $usuario_id;
+
     /**
      * Create a new notification instance.
      */
-    public function __construct()
+    public function __construct($id_vacante, $nombre_vacante, $usuario_id)
     {
         //
+        $this->id_vacante = $id_vacante;
+        $this->nombre_vacante = $nombre_vacante;
+        $this->usuario_id = $usuario_id;
     }
 
     /**
@@ -26,7 +33,7 @@ class NuevoCandidato extends Notification
      */
     public function via(object $notifiable): array
     {
-        return ['mail'];
+        return ['mail', 'database'];
     }
 
     /**
@@ -34,10 +41,13 @@ class NuevoCandidato extends Notification
      */
     public function toMail(object $notifiable): MailMessage
     {
+        $url = url('/notificaciones');
+
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+                    ->line('Has recibido un nuevo candidato en tu vacante.')
+                    ->line('La vacante es: ' . $this->nombre_vacante)
+                    ->action('Ver notificaciones', $url)
+                    ->line('¡Gracias por utilizar DevJobs!');
     }
 
     /**
@@ -54,6 +64,10 @@ class NuevoCandidato extends Notification
 
     //Almacena las notificaciones en la BD
     public function toDatabase($notifiable){
-
+        return [
+            'id_vacante' => $this->id_vacante,
+            'nombre_vacante' => $this->nombre_vacante,
+            'usuario_id' => $this->usuario_id
+        ];
     }
 }
