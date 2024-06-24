7097e6898951c62b00b3aa0c150eccff3bf5c86c
petereycoder <fp.pipe.reyes@gmail.com>
2024-06-24 14:13:02 -0600

    Se creó la funcionalidad de listar candidatos

--- app/Models/Vacante.php
+++ app/Models/Vacante.php
@@ -31,7 +31,7 @@ class Vacante extends Model
     }
 
     public function candidatos(){
-        return $this->hasMany(Candidato::class);
+        return $this->hasMany(Candidato::class)->orderBy('created_at', 'DESC');
     }
 
     public function reclutador(){
--- resources/views/notificaciones/index.blade.php
+++ resources/views/notificaciones/index.blade.php
@@ -11,8 +11,9 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <h1 class="text-2xl font-bold text-center my-10">Mis Notificaciones</h1>
                     
-                    @forelse ($notificaciones as $notificacion)
-                        <div class="p-5 border border-gray-200 lg:flex justify-between lg:items-center">
+                    <div class="divide-y divide-gray-200">
+                        @forelse ($notificaciones as $notificacion)
+                        <div class="p-5 lg:flex justify-between lg:items-center">
                             <div>
                                 <p>Tienes un nuevo candidato en: 
                                     <span class="font-bold">{{ $notificacion->data['nombre_vacante'] }}</span>
@@ -22,14 +23,15 @@
                                 </p>
                             </div>
                             <div class="mt-5 lg:mt-0">
-                                <a href="#" class="bg-indigo-500 p-3 text-sm uppercase font-bold text-white rounded-lg">
+                                <a href="{{route('candidatos.index', $notificacion->data['id_vacante'])}}" class="bg-indigo-500 p-3 text-sm uppercase font-bold text-white rounded-lg">
                                     Ver Candidatos
                                 </a>
                             </div>
                         </div>
-                    @empty
-                        <p class="text-center text-gray-600">No hay notificaciones nuevas</p>
-                    @endforelse
+                        @empty
+                            <p class="text-center text-gray-600">No hay notificaciones nuevas</p>
+                        @endforelse
+                    </div>
                     
                 </div>
             </div>
